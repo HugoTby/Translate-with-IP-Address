a3dfb210ca901c5b93bfb487b700206c9e25b069
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-02-15 20:20:52 +0100

    Update languages_list.php

--- languages_list.php
+++ languages_list.php
@@ -1,11 +1,13 @@
 <?php
-// Tableau de correspondance entre les codes de pays et les codes de langue
+// [FR] Tableau de correspondance entre les codes de pays et les codes de langue.
+// [EN] Table of correspondence between country codes and language codes.
 $languagesByCountry = array(
     'NL' => 'nl', // Pays-Bas
     'FR' => 'fr', // France
     'JP' => 'jp', // Japon
     'US' => 'us', // États-unis
-    // ajouter d'autres pays et leurs langues associées ici
+    // [FR] Ajoutez d'autres pays et leurs langues associées ici
+    // [EN] Add more countries and their associated languages here
 );
 
 ?>
